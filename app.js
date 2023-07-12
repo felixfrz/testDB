8407a3063278f0ff9fde409e60dff9b2b5a5912b
Ayodeji Felix Oladoyin <oladoyinayodeji@gmail.com>
2023-07-12 21:52:17 +0100

    api

--- app.js
+++ app.js
@@ -1,48 +1,46 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const mysql = require("mysql");
-const cors = require("cors");
-require("./db/conn");
-const router = require("./Router/router");
-const bodyParser = require("body-parser"); router.use(bodyParser.json());
-const cookieParser = require('cookie-parser'); 
+const mysql = require('mysql');
+const cors = require('cors');
+require('./db/conn');
+const router = require('./Router/router');
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
+const cookieParser = require('cookie-parser');
 const session = require('express-session');
 
 const PORT = process.env.PORT || 3001;
 
-app.get("/", (req,res)=>{
-    res.send("server start")
+app.get('/', (req, res) => {
+	res.send('server start');
 });
 
-
-app.use(express.json())
+app.use(express.json());
 app.use(
 	cors({
 		origin: ['https://scms-web.netlify.app'],
-		methods: ['GET', 'POST'],
+		methods: ['GET', 'POST', 'DELETE'],
 		credentials: true,
 	})
 );
 
 app.use(cookieParser());
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.use(session({
-
-    key: "user",
-    secret: "jishank",
-    resave: "false",
-    saveUninitialized: false,
-    cookie: {
-        expires: 60 * 60 * 24,
-    },
-
-})
+app.use(
+	session({
+		key: 'user',
+		secret: 'jishank',
+		resave: 'false',
+		saveUninitialized: false,
+		cookie: {
+			expires: 60 * 60 * 24,
+		},
+	})
 );
 
-app.use("/uploads", express.static("./uploads"))
+app.use('/uploads', express.static('./uploads'));
 app.use(router);
 
 app.listen(PORT, () => {
